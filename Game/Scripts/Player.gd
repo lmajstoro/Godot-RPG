212ad3f59f1bcae61348d52a72b84846fa906a6e
Luka Majstorovic <luka.majstorovic321@gmal.com>
2022-02-08 15:08:04 +0100

    Created movement animations, setup test animations

--- Game/Scripts/Player.gd
+++ Game/Scripts/Player.gd
@@ -6,21 +6,27 @@ const FRICTION = 400
 
 var velocity = Vector2.ZERO
 
+#onready var creates once node is loaded
+#with $ you access child nodes
+onready var animationPlayer = $AnimationPlayer
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	#Normalize vector to fix movement to botom-right and top-left
-	#Equalize speed of right, left, up, down
+
 	input_vector = input_vector.normalized()
-	
-	#All player action should be multiplied by delta(time between 2 frames)
-	#delta is usually 1/60
+
 	if input_vector != Vector2.ZERO:
+		#todo fix with animation tree
+		if input_vector.x > 0:
+			animationPlayer.play("run_right")
+		else:
+			animationPlayer.play("run_left")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationPlayer.play("idle_right")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
-	#move_and_slide applies delta on forwarded velocity, no need to do it again
+		
 	velocity = move_and_slide(velocity)
