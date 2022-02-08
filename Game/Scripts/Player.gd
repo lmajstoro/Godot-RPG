2cd8584ed0846f5e3ed6457f0680a50a7844b0ed
Luka Majstorovic <luka.majstorovic321@gmal.com>
2022-02-08 21:08:34 +0100

    Transformed all movement and idling animations into animation tree

--- Game/Scripts/Player.gd
+++ Game/Scripts/Player.gd
@@ -3,12 +3,16 @@ extends KinematicBody2D
 const MAX_SPEED = 80
 const ACCELERATION = 400
 const FRICTION = 400
+const IDLE_BLEND = "parameters/Idle/blend_position"
+const MOVEMENT_BLEND = "parameters/Movement/blend_position"
+const PLAYBACK = "parameters/playback"
 
 var velocity = Vector2.ZERO
 
-#onready var creates once node is loaded
 #with $ you access child nodes
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get(PLAYBACK)
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -19,14 +23,16 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
-		#todo fix with animation tree
-		if input_vector.x > 0:
-			animationPlayer.play("run_right")
-		else:
-			animationPlayer.play("run_left")
+		#Set blend position for idle via forwarding input vector
+		animationTree.set(IDLE_BLEND, input_vector)
+		#Set blend position for movement via forwarding input vector
+		animationTree.set(MOVEMENT_BLEND, input_vector)
+		#When moving, set state to Movement
+		animationState.travel("Movement")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animationPlayer.play("idle_right")
+		#When idling, set state to Idle
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
