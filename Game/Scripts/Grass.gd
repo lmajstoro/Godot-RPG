887161d11d51db046683c33599868deb5dbd8886
Luka Majstorovic <luka.majstorovic321@gmal.com>
2022-02-10 16:57:29 +0100

    Small fix in Grass.gd script, added destroy to special attack

--- Game/Scripts/Grass.gd
+++ Game/Scripts/Grass.gd
@@ -2,17 +2,19 @@ extends Node2D
 
 func _process(delta):
 	if Input.is_action_just_pressed("attack"):
-		#Load and create instance of scene (destroy animation)
-		var GrassDestroyEffect = load("res://Scenes/GrassDestroyEffect.tscn")
-		var grassDestroyEffect = GrassDestroyEffect.instance()
-		#fetch main sceen (World)
-		var world = get_tree().current_scene
-		#attach destroy animation as child to main scene
-		world.add_child(grassDestroyEffect)
-		#position destroy animation on grass position
-		grassDestroyEffect.position = self.position
-		#destroy static grass
-		queue_free()
+		_grassBehaviour()
+	elif Input.is_action_just_pressed("specialAttack"):
+		_grassBehaviour()
 		
-		
-
+func _grassBehaviour():
+	#Load and create instance of scene (destroy animation)
+	var GrassDestroyEffect = load("res://Scenes/GrassDestroyEffect.tscn")
+	var grassDestroyEffect = GrassDestroyEffect.instance()
+	#fetch main sceen (World)
+	var world = get_tree().current_scene
+	#attach destroy animation as child to main scene
+	world.add_child(grassDestroyEffect)
+	#position destroy animation on grass position
+	grassDestroyEffect.position = self.position
+	#destroy static grass
+	queue_free()
